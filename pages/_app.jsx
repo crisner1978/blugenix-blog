f829cd9a212f708cccf969897dce48ccc0557427
Chris Risner <crisner.prsvr@gmail.com>
2023-04-10 14:29:33 -0500

    edit _app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,8 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 import { AppProvider } from 'components'
 import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
