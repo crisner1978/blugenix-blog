fdfbd384d828cfe2969cea53d7a24e907ad8103c
Chris Risner <crisner.prsvr@gmail.com>
2023-05-08 15:38:13 -0500

    update blog date

--- components/blog/PostWidget.js
+++ components/blog/PostWidget.js
@@ -15,23 +15,25 @@ const PostWidget = ({ categories, slug }) => {
   })
 
   return (
-    <div className='bg-white dark:bg-stone-800 p-8 mb-8 shadow-lg rounded-lg'>
-      <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
-        {slug ? "Related Posts" : "Recent Posts"}
+    <div className="mb-8 rounded-lg bg-white p-8 shadow-lg dark:bg-stone-800">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+        {slug ? 'Related Posts' : 'Recent Posts'}
       </h3>
       {data?.map((post) => (
         <Link key={post.id} href={`/post/${post.slug}`}>
-          <div className='widgets'>
-            <div className='w-20 flex-none'>
-              <img className='rounded-lg align-middle' src={post.featuredImage.url} alt={post.title} />
+          <div className="widgets">
+            <div className="w-20 flex-none">
+              <img
+                className="rounded-lg align-middle"
+                src={post.featuredImage.url}
+                alt={post.title}
+              />
             </div>
-            <div className='flex-grow ml-4'>
-              <p className='text-gray-500 dark:text-gray-100 text-xs'>
-                {moment(post.createAt).format('MMM DD, YYYY')}
+            <div className="ml-4 flex-grow">
+              <p className="text-xs text-gray-500 dark:text-gray-100">
+                {moment(post.createdAt).format('MMM DD, YYYY')}
               </p>
-              <p className='text-base'>
-                {post.title}
-              </p>
+              <p className="text-base">{post.title}</p>
             </div>
           </div>
         </Link>
